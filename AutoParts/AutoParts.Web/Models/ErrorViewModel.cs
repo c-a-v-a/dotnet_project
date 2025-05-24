914609b42cbf0b7c97cbd7f011cb854b86962ddc
c-a-v-a <cavaevilmail@gmail.com>
2025-05-24 13:49:41 +0200

    init

---
+++ AutoParts/AutoParts.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AutoParts.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "AutoPartsStore";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { system = system; config.allowUnfree = true; };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              dotnet-sdk
+              dotnetPackages.Nuget
+              unzip
+              zip
+              postman
+              tree
+            ];
+          };
+        }
+      );
+}

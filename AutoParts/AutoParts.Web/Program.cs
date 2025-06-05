f047ac4c6e825505ed0a39444bf7ff57cd71c884
Frixu <bartekciesla554@gmail.com>
2025-06-05 14:53:23 +0200

    Pierwsze zmiany dla US4

--- AutoParts/AutoParts.Web/Program.cs
+++ AutoParts/AutoParts.Web/Program.cs
@@ -7,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();

eaa5a0b04895b78a7eb8aa425c186e8dd61ce7c3
c-a-v-a <cavaevilmail@gmail.com>
2025-06-07 00:34:53 +0200

    Finished US2

---
+++ AutoParts/AutoParts.Web/Authorization/RoleRequirement.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoParts.Web.Authorization;
+
+using AutoParts.Web.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+public class RoleRequirement : IAuthorizationRequirement
+{
+    public UserRole RequiredRole { get; }
+    public RoleRequirement(UserRole role) => RequiredRole = role;
+}

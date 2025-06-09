05085eac7c5e5f4b6c6e39993311f43baea17b81
Frixu <bartekciesla554@gmail.com>
2025-06-09 16:20:18 +0200

    Moze jedynie frontend do poprawy i nie wiem jak z wyborem statusu

---
+++ AutoParts/AutoParts.Web/Migrations/20250609140954_FixServiceOrderSchema.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoParts.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixServiceOrderSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceOrders_Customers_CustomerId",
+                table: "ServiceOrders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceOrders_Vehicles_VehicleId",
+                table: "ServiceOrders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServiceOrders_CustomerId",
+                table: "ServiceOrders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServiceOrders_VehicleId",
+                table: "ServiceOrders");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "ServiceOrders");
+
+            migrationBuilder.DropColumn(
+                name: "VehicleId",
+                table: "ServiceOrders");
+        }
+    }
+}

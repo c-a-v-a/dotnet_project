f1708ac6a4f575aa59195ced1d7302d3e019a064
Frixu <bartekciesla554@gmail.com>
2025-06-16 00:18:46 +0200

    saq

---
+++ AutoParts/AutoParts.Web/Configuration/ReportEmailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoParts.Web.Configuration
+{
+    public class ReportEmailSettings
+    {
+        public string Recipient { get; set; } = "";
+        public string Sender { get; set; } = "";
+        public string SmtpHost { get; set; } = "";
+        public int SmtpPort { get; set; }
+        public string SmtpUser { get; set; } = "";
+        public string SmtpPass { get; set; } = "";
+    }
+}

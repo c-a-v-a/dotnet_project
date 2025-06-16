553ee63596f56f508e4176d66d3c4f7d2ea30995
c-a-v-a <cavaevilmail@gmail.com>
2025-06-16 02:16:32 +0200

    Add vehicle api controller, service and unit tests

--- AutoParts/AutoParts.Web/Mappers/VehicleMapper.cs
+++ AutoParts/AutoParts.Web/Mappers/VehicleMapper.cs
@@ -10,8 +10,11 @@ public partial class VehicleMapper
 {
     public partial VehicleModel ToViewModel(Vehicle vehicle);
 
+    [MapperIgnoreTarget(nameof(Vehicle.Id))]
+    [MapperIgnoreSource(nameof(VehicleModel.Id))]
     public partial Vehicle ToEntity(VehicleModel model);
 
+    [MapperIgnoreTarget(nameof(Vehicle.Customer))]
     public partial void ToEntity(VehicleModel source, [MappingTarget] Vehicle target);
 
     public partial CustomerShortDto ToShortDto(Customer customer);
